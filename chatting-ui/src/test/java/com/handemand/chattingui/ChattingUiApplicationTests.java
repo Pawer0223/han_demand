7c9ef300c3f2c17b088252b3bc6d4024e8623356
mountain <pawer02023@gmail.com>
2022-03-15 15:12:32 +0900

    chore: 디렉터리추가, user-service 삭제 (#10)
    * feat: user-service, chatting-service gateway랑 연동, 화면은 ui 생성, cors 처리
    
    * chore: 디렉터리가 추가 안되어서 추가, 기존 user-service 제거

---
+++ chatting-ui/src/test/java/com/handemand/chattingui/ChattingUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handemand.chattingui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
