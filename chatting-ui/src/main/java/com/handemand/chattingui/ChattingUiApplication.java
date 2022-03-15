e7057dbd2bcc8fe5cee6d637302ad3a759852618
taesan <pawer02023@gmail.com>
2022-03-15 15:09:00 +0900

    chore: 디렉터리가 추가 안되어서 추가, 기존 user-service 제거

---
+++ chatting-ui/src/main/java/com/handemand/chattingui/ChattingUiApplication.java
@@ -0,0 +1,13 @@
+package com.handemand.chattingui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingUiApplication.class, args);
+    }
+
+}
